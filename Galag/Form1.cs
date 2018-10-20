fced4a4c6b9f10adafd89f466f44dc4b6b473b8f
SK O <mnbvcxzjjkk312@hotmail.com>
2018-10-20 22:33:59 +0900

    181020 오후 10시 30분 오성규 1. 제작하던 파일 복사 및 공유.

---
+++ Galag/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Galag
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                Point p = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+                pictureBox1.Location = p;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                Point p = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+                pictureBox1.Location = p;
+            }
+        }
+    }
+}
