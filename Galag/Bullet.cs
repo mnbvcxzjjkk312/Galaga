7450132ea65df3802ffcd4be5e2f9a4c46df9b22
Tillink <41276552+Tillink@users.noreply.github.com>
2018-10-22 22:18:03 +0900

    18-10-22 오후 10:15 김소현  1. 총알 이미지 플레이어 중간 적용  2. 총알 이동 추가해야함.  3. PictureBox 이름 -> PlayerPB와 BulletPB로 임시 명명

--- Galag/Bullet.cs
+++ Galag/Bullet.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Galag
 {
     public class Bullet
     {
+        private PictureBox _BulletPb;
+        private PictureBox _PlayerPb;
+
         
+
     }
 }
