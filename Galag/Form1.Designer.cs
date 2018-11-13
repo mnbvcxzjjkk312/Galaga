71ae0f91192517c78ef907c23ba2493f5c15b2dd
SK O <mnbvcxzjjkk312@hotmail.com>
2018-11-14 08:15:26 +0900

    181113 오전 3시 새로 짠 코드.

--- Galag/Form1.Designer.cs
+++ Galag/Form1.Designer.cs
@@ -30,45 +30,44 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.PlayerPB = new System.Windows.Forms.PictureBox();
-            this.BulletPB = new System.Windows.Forms.PictureBox();
+            this.Bullet = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.PlayerPB)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.BulletPB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bullet)).BeginInit();
             this.SuspendLayout();
             // 
             // PlayerPB
             // 
             this.PlayerPB.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.PlayerPB.Image = ((System.Drawing.Image)(resources.GetObject("PlayerPB.Image")));
-            this.PlayerPB.Location = new System.Drawing.Point(147, 504);
+            this.PlayerPB.Location = new System.Drawing.Point(147, 538);
             this.PlayerPB.Name = "PlayerPB";
-            this.PlayerPB.Size = new System.Drawing.Size(48, 48);
+            this.PlayerPB.Size = new System.Drawing.Size(48, 51);
             this.PlayerPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.PlayerPB.TabIndex = 0;
             this.PlayerPB.TabStop = false;
             // 
-            // BulletPB
+            // Bullet
             // 
-            this.BulletPB.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BulletPB.BackgroundImage")));
-            this.BulletPB.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.BulletPB.Location = new System.Drawing.Point(162, 478);
-            this.BulletPB.Name = "BulletPB";
-            this.BulletPB.Size = new System.Drawing.Size(20, 20);
-            this.BulletPB.TabIndex = 1;
-            this.BulletPB.TabStop = false;
+            this.Bullet.Image = global::Galag.Properties.Resources.Bullet3;
+            this.Bullet.Location = new System.Drawing.Point(165, 505);
+            this.Bullet.Name = "Bullet";
+            this.Bullet.Size = new System.Drawing.Size(10, 27);
+            this.Bullet.TabIndex = 1;
+            this.Bullet.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(332, 553);
-            this.Controls.Add(this.BulletPB);
+            this.ClientSize = new System.Drawing.Size(332, 590);
+            this.Controls.Add(this.Bullet);
             this.Controls.Add(this.PlayerPB);
             this.Name = "Form1";
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.PlayerPB)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.BulletPB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bullet)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -76,7 +75,7 @@
         #endregion
 
         private System.Windows.Forms.PictureBox PlayerPB;
-        private System.Windows.Forms.PictureBox BulletPB;
+        private System.Windows.Forms.PictureBox Bullet;
     }
 }
 
--- Galag/Form1.cs
+++ Galag/Form1.cs
@@ -15,103 +15,36 @@ namespace Galag
         public Form1()
         {
             InitializeComponent();
-            BulletPB.Visible = false;
-            // bullet Image 
-            bullet = new Bitmap(GetType(), "Bullet3.png");
         }
-
-        Rectangle bullet1 = new Rectangle();
-
-        // 총알 위치
-        private void BulletLocation()
-        {
-            
-        }
-        public Bitmap bullet;
-        // bullet x,y 좌표와 Width, Height
-        int bulletX;
-        int bulletY;
-        int bulletW;
-        int bulletH;
-
-        // Form x, y 좌표
-        int FormX = 350;
-        int FormY = 600;
-
-        // Player x, y 좌표
-        int playerX;
-        int playerY;
-
-        //플레이어 W, H
-        //playerWidth = PlayerPB.Size.Width;
-        //playerHeight = PlayerPB.Size.Height;
-
-        // Player 가로, 세로 길이
-        int playerWidth;
-        int playerHeight;
-        
-
-        bool hasBullet = true;
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
+            Point p;
+            int a = 0;
+            while (a < 10)
+            {
+                a -= 5;
+            }
+
             if (e.KeyCode == Keys.Right)
             {
-                Point p = new Point(PlayerPB.Location.X + 5, PlayerPB.Location.Y);
+                p = new Point(PlayerPB.Location.X + 5, PlayerPB.Location.Y);
                 PlayerPB.Location = p;
             }
-
             else if (e.KeyCode == Keys.Left)
             {
-                Point p = new Point(PlayerPB.Location.X - 5, PlayerPB.Location.Y);
+                p = new Point(PlayerPB.Location.X - 5, PlayerPB.Location.Y);
                 PlayerPB.Location = p;
             }
 
-            // space bar를 누르면 
-            // 1. 비행기의 앞에서 총알이 생성된다.
-            // 2. 총알은 화면 안에서 1개만 사용할 수 있다.
-            // 3. 총알은 화면 끝까지 가면 소멸한다.
-            // 4. 화면 안에 총알이 있을 경우 두번째 총알은 나오지 않는다. 
-            // 5. 
             if (e.KeyCode == Keys.Space)
             {
-                hasBullet = false;
-                BulletPB.Visible = true;
-
-                BulletLocation();
+                while (Bullet.Location.Y > 100)
+                {
+                    Point pb = new Point(Bullet.Location.X, Bullet.Location.Y - 5);
+                    Bullet.Location = pb;
+                }
             }
         }
-
-        public void BulletLocation()
-        {
-            Point point = new Point();
-
-            int bulletY = BulletPB.Location.Y;
-            int bulletX = BulletPB.Location.X;
-
-            point.X = PlayerPB.Location.X + 10;
-            point.Y = PlayerPB.Location.Y - playerHeight;
-            
-
-            Point p = new Point(point.X, point.Y);
-            BulletPB.Location = p;
-
-            // BulletLocation(); //총알 위치
-            while (BulletPB.Location.Y <= FormY && !hasBullet)
-            {
-                point.X = PlayerPB.Location.X;
-                point.Y += 10;
-
-                p = new Point(point.X, point.Y);
-                BulletPB.Location = p;
-            }
-
-            if (BulletPB.Location.Y >= FormY && BulletPB.Visible && !hasBullet)
-            {
-                BulletPB.Visible = false;
-                hasBullet = true;
-            }
-        }
-        
     }
 }
